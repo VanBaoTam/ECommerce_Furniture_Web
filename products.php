14a4c455ad6659f11105cff7bbc15f53ab609bbb
VanBaoTam <vanbaotam187@gmail.com>
2023-11-05 20:16:29 +0700

    add:feat/fetching-products

--- products.php
+++ products.php
@@ -84,83 +84,11 @@
   <div class="products">
     <div class="container">
       <div class="row">
-        <div class="col-md-4">
-          <div class="product-item">
-            <a href="product-details.php"><img src="assets/images/product-1-370x270.jpg" alt=""></a>
-            <div class="down-content">
-              <a href="product-details.php">
-                <h4>Creamy Armchair.</h4>
-              </a>
-              <h6><small><del>$399.00 </del></small> $299.00</h6>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dicta voluptas quia dolor fuga odit.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="product-item">
-            <a href="product-details.php"><img src="assets/images/product-2-370x270.jpg" alt=""></a>
-            <div class="down-content">
-              <a href="product-details.php">
-                <h4>Light Pink Armchair.</h4>
-              </a>
-              <h6><small><del>$200.00</del></small> $149.00</h6>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non beatae soluta, placeat vitae cum maxime
-                culpa itaque minima.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="product-item">
-            <a href="product-details.php"><img src="assets/images/product-3-370x270.jpg" alt=""></a>
-            <div class="down-content">
-              <a href="product-details.php">
-                <h4>Oak Stool.</h4>
-              </a>
-              <h6><small><del>$80.00</del></small> $49.00</h6>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt nisi quia aspernatur, harum facere
-                delectus saepe enim?</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="product-item">
-            <a href="product-details.php"><img src="assets/images/product-4-370x270.jpg" alt=""></a>
-            <div class="down-content">
-              <a href="product-details.php">
-                <h4>Green Wool Couch.</h4>
-              </a>
-              <h6><small><del>$999.00 </del></small> $779.00</h6>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dicta voluptas quia dolor fuga odit.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="product-item">
-            <a href="product-details.php"><img src="assets/images/product-5-370x270.jpg" alt=""></a>
-            <div class="down-content">
-              <a href="product-details.php">
-                <h4>Real Leather Couch.</h4>
-              </a>
-              <h6><small><del>$99.00</del></small> $79.00</h6>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non beatae soluta, placeat vitae cum maxime
-                culpa itaque minima.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="product-item">
-            <a href="product-details.php"><img src="assets/images/product-6-370x270.jpg" alt=""></a>
-            <div class="down-content">
-              <a href="product-details.php">
-                <h4>Decorative Desk.</h4>
-              </a>
-              <h6><small><del>$1999.00</del></small> $1779.00</h6>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt nisi quia aspernatur, harum facere
-                delectus saepe enim?</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-12">
+        <?php
+        include("./modules/products/fetching.php");
+        ?>
+        <!-- Pagination -->
+        <!-- <div class="col-md-12">
           <ul class="pages">
             <li class="active"><a href="#">1</a></li>
             <li><a href="#">2</a></li>
@@ -168,7 +96,7 @@
             <li><a href="#">4</a></li>
             <li><a href="#"><i class="fa fa-angle-double-right"></i></a></li>
           </ul>
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
